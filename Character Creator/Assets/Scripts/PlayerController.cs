9496c56394baed9f717ac85b6aab54d18052da52
Emilianog10 <89863339+Emilianog10@users.noreply.github.com>
2021-12-07 10:05:24 -0800

    Revert "Revert "feat: Add No Armor button""
    This reverts commit 594bfa72da55eacaf0834a6e4b5e9cdf1dcb3347.

--- Character Creator/Assets/Scripts/PlayerController.cs
+++ Character Creator/Assets/Scripts/PlayerController.cs
@@ -4,6 +4,12 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+
+    public SpriteRenderer ArmourRenderer;
+    public void SetArmor(Sprite newSprite)
+    {
+        ArmourRenderer.sprite = newSprite;
+    }
     // Start is called before the first frame update
     void Start()
     {
